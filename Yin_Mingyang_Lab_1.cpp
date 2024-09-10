7a1055e0e1de5b6807485ab6bc128a34496f9985
Mingyangyin48 <mingyangy@nycstudents.net>
2024-09-10 17:10:03 -0400

    Lab 1

---
+++ Yin_Mingyang_Lab_1.cpp
@@ -0,0 +1,45 @@
+// Mingyang Yin
+// Lab 1
+// Sept. 10, 2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout <<"#####################################################################################"<< endl; // The first section of hashtags.
+    cout <<"#                                                                                   #"<< endl; // The space between the sections there is the hashtags that border the code incoprated into the code.
+    cout <<"# Ways to access the Task Manager on your Windows computer:                         #"<< endl; // First line of text. 
+    cout <<"#                                                                                   #"<< endl;
+    cout <<"#        Press the key combination Ctrl + Shift + Escape                            #"<< endl; // Second line of text.
+    cout <<"#                                                                                   #"<< endl;
+    cout <<"#        Press the key combination Ctrl + Alt + Delete and select “Task Manager”    #"<< endl; // Third line of text.
+    cout <<"#                                                                                   #"<< endl;
+    cout <<"#        Type “Task Manager” in the Windows Start menu search                       #"<< endl; // Last line of text.
+    cout <<"#                                                                                   #"<< endl;
+    cout <<"#####################################################################################"<< endl;
+
+    return 0;
+
+}
+    
+/*
+
+[Running] cd "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Mingyangyin48\" && g++ Yin_Mingyang_Lab_1.cpp -o Yin_Mingyang_Lab_1 && "c:\Users\College\Documents\GitHub\lab-1-cout-statements-Mingyangyin48\"Yin_Mingyang_Lab_1
+#####################################################################################
+#                                                                                   #
+# Ways to access the Task Manager on your Windows computer:                         #
+#                                                                                   #
+#        Press the key combination Ctrl + Shift + Escape                            #
+#                                                                                   #
+#        Press the key combination Ctrl + Alt + Delete and select “Task Manager”    #
+#                                                                                   #
+#        Type “Task Manager” in the Windows Start menu search                       #
+#                                                                                   #
+#####################################################################################
+
+[Done] exited with code=0 in 1.468 seconds
+
+*/
